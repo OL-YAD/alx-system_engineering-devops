e782f5fd8838f0e3bfe4fa7a22851da5c661280a
OL-YAD <olyadtemesgen@gmail.com>
2022-11-02 21:43:49 +0300

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -24,4 +24,3 @@ def number_of_subscribers(subreddit):
 #     print("Please pass an argument for the subreddit to search.")
 # else:
 #     print("{:d}".format(number_of_subscribers(sys.argv[1])))
-
